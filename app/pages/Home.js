1cc047a47ef096f3851006ba9113955affad185e
liaopeng541 <36416976@qq.com>
2018-01-27 23:55:49 +0800

    增加了下拉刷新第三方组件

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -2,31 +2,133 @@
  * Created by liaopeng on 2018/1/25.
  */
 import React from 'react';
-import {Text,View,ScrollView} from 'react-native';
+import {Text, View, ScrollView,InteractionManager} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
 import {connect} from "react-redux";
 import Page from "../components/Page"
 import common from "../common/common"
-class Home extends React.Component{
+class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.F=new common(this)
-        this.state={
+        this.F = new common(this)
+        this.state = {
             isloading: false,
             isRefreshing: false,
-            v:"我的小小数据"
+            v: "我的小小数据",
+            title:"",
+            en:true,
+
+
         }
     }
-    componentDidMount()
+    rendrow()
     {
+        var content=[];
+        for(var i=1;i<50;i++)
+        {
+            content.push(
+                <View key={i} style={{
+                height: 80,
+                borderWidth: 1,
+                borderColor: "#999999",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Text>{i}{i}{i}{i}</Text>
+            </View>)
+        }
+        return content
     }
+    componentDidMount() {
+        this.on=false;
+        setTimeout(()=>{
+            this.r.scrollTo({x: 0, y: 150, animated: false})
+        },0)
+    }
+
+    onScroll(e) {
+        this.y=e.nativeEvent.contentOffset.y;
+        if(!this.on && this.y>=150)
+        {
+            this.sback=false;
+        }
+        if(!this.on && this.y<=150 && !this.sback)
+        {
+            this.r.scrollTo({x: 0, y: 150, animated: true})
+            this.sback=true;
+        }
+        if(this.on && this.y<=130)
+        {
+            this.setState({
+                title:"下拉可以刷新"
+            })
+        }
+        if(this.on && this.y<=75)
+        {
+            this.setState({
+                title:"可以了，松手吧"
+            })
+        }
+        if(this.on && this.y<=10)
+        {
+            this.setState({
+                title:"哥，松手吧，真拉不动了"
+            })
+
+        }
+    }
+    onTouchStart()
+    {
+        this.on=true;
+    }
+    onend()
+    {
+        this.on=false;
+        if(this.y<=75)
+        {
+            this.r.scrollTo({x: 0, y: 75, animated: true})
+            this.setState({title:"正在刷新中"})
+            if(!this.time){
+                this.time = setTimeout(()=>{
+                    if(!this.on){this.r.scrollTo({x: 0, y: 150, animated: true})}
+                    this.setState({title:""})
+                    this.time=null;
+                },3000)
+            }
+
+
+        }else if(this.y<=150){
+            this.r.scrollTo({x: 0, y: 150, animated: true})
+        }
+    }
+
     render() {
         return (
-            <Page showLoading={this.state.isloading} style={{paddingTop:200}}>
-                <Text onPress={this.F.to.bind(this,"My",{aa:"aaaa"})}>{this.state.v}</Text>
-                <Text
-                    onPress={this.F.showLoading.bind(this,this)}
-                >还原222</Text>
+            <Page showLoading={this.state.isloading}>
+                <ScrollView
+                    ref={(r)=>{this.r=r}}
+                    onScroll={this.onScroll.bind(this)}
+                    onTouchStart={this.onTouchStart.bind(this)}
+                    onScrollEndDrag={this.onend.bind(this)}
+                    showsVerticalScrollIndicator={false}
+                    scrollEventThrottle={16}
+                  //  scrollEnabled={this.state.en}
+                    bounces={false}
+
+                >
+                    <View style={{
+                    height: 150,
+                    justifyContent: "flex-end",
+                    alignItems: "center"
+                }}>
+                      <View>
+                    <Text>{this.state.title}</Text>
+                     </View>
+                </View>
+
+                    {this.rendrow()}
+
+                </ScrollView>
             </Page>
         );
     }
--- app/pages/List.js
+++ app/pages/List.js
@@ -2,9 +2,15 @@
  * Created by liaopeng on 2018/1/25.
  */
 import React from 'react';
-import {Text,View} from 'react-native';
+import {Text,View,ScrollView} from 'react-native';
 import {connect} from "react-redux";
 import common from "../common/common";
+import {
+    SwRefreshScrollView, //支持下拉刷新的ScrollView
+    SwRefreshListView, //支持下拉刷新和上拉加载的ListView
+    RefreshStatus, //刷新状态 用于自定义下拉刷新视图时使用
+    LoadMoreStatus //上拉加载状态 用于自定义上拉加载视图时使用
+} from 'react-native-swRefresh'
 class List extends React.Component {
     constructor(props) {
         super(props);
@@ -13,19 +19,44 @@ class List extends React.Component {
             aaa:this.params?this.params.aaa:"000"
         }
     }
+    rendrow()
+    {
+        var content=[];
+        for(var i=1;i<50;i++)
+        {
+            content.push(
+                <View key={i} style={{
+                    height: 80,
+                    borderWidth: 1,
+                    borderColor: "#999999",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                    <Text>{i}{i}{i}{i}</Text>
+                </View>)
+        }
+        return content
+    }
     render() {
         return (
-            <View style={{backgroundColor:"#cccccc",flex:1,alignItems:"center",justifyContent:"center"}}>
-            <Text onPress={
-                ()=>{
-                    this.F.to("My")
+                <SwRefreshScrollView
+                    //bounces={false}
+                    customRefreshView={(refresStatus,offsetY)=>{
+                        console.log(offsetY)
+                        return (
+                            <View style={{height:100,alignItems:"center",justifyContent:"flex-end"}}>
+                                <Text>{'状态:'+refresStatus+','+offsetY}</Text>
+                            </View>)
+                    }} //自定义下拉刷新视图参数，refresStatus是上面引入的RefreshStatus类型，对应刷新状态各个状态。offsetY对应下拉的偏移量,可用于定制动画。自定义视图必须通过customRefreshViewHeight指定高度
 
-            }}> List 555! </Text>
-                <Text onPress={
-                    ()=>{
-                    }}> {this.state.aaa} </Text>
+                    customRefreshViewHeight={100} //自定义刷新视图时必须指定高度
+                >
 
-            </View>
+                    {this.rendrow()}
+
+                </SwRefreshScrollView>
+
+
         );
     }
 
